d667cc92146c1d4b98845dbba8164b5ebd951ed0
yodeng <yodeng@tju.edu.cn>
2022-08-14 17:31:45 +0800

    first commit

---
+++ src/src.py
@@ -0,0 +1,68 @@
+import os
+import re
+
+from .utils import *
+
+
+class BuildGCC(Utils):
+
+    def __init__(self, version, download_dir, verbose=True, threads=20):
+        self.version = re.match("^\d", version) and "gcc-" + version or version
+        self.download_dir = os.path.abspath(download_dir)
+        self.verbose = verbose
+        self.threads = threads
+
+    def download_gcc(self):
+        url = os.path.join(gcc_base_url, self.version,
+                           self.version + ".tar.gz")
+        self.mkdirs(self.download_dir)
+        filename = os.path.basename(url)
+        outfile = os.path.join(self.download_dir, filename)
+        if not os.path.isfile(outfile) or os.path.isfile(outfile+".ht"):
+            hget(url=url, outfile=os.path.join(self.download_dir,
+                 filename), quite=True, timeout=30)
+        self.uncompress(os.path.join(self.download_dir, filename),
+                        self.download_dir, verbose=False)
+        with open(os.path.join(self.download_dir, self.version, "contrib/download_prerequisites")) as fo:
+            ctx = fo.read()
+        try:
+            base_url = re.findall("\s+base_url='(.+)'\s+", ctx)[0]
+            gmp = re.findall("\s+gmp='(gmp-.+)'\s+", ctx)[0]
+            mpfr = re.findall("\s+mpfr='(mpfr-.+)'\s+", ctx)[0]
+            mpc = re.findall("\s+mpc='(mpc-.+)'\s+", ctx)[0]
+            isl = re.findall("\s+isl='(isl-.+)'\s+", ctx)[0]
+        except:
+            base_url = re.findall("wget (.+?)\s+", ctx)
+            gmp = re.findall("\s+GMP=(gmp-.+)\s", ctx)[0]
+            mpfr = re.findall("\s+MPFR=(mpfr-.+)\s", ctx)[0]
+            mpc = re.findall("\s+MPC=(mpc-.+)\s", ctx)[0]
+            isl = re.findall("\s+ISL=(isl-.+)\s", ctx)[0]
+            for i, _ in enumerate(base_url):
+                t = string.Template(base_url[i])
+                u = t.safe_substitute(
+                    {"GMP": gmp, "MPFR": mpfr, "MPC": mpc, "ISL": isl})
+                base_url[i] = u
+            gmp, mpfr, mpc, isl = base_url
+        for n in [gmp, mpfr, mpc, isl]:
+            if ":" in n:
+                url = n
+            else:
+                url = os.path.join(base_url, n)
+            n = os.path.basename(url)
+            outfile = os.path.join(
+                self.download_dir, self.version, os.path.basename(url))
+            if not os.path.isfile(outfile) or os.path.isfile(outfile+".ht"):
+                hget(url=url, outfile=outfile, timeout=30, quite=True)
+            self.uncompress(os.path.join(self.download_dir, self.version, n), os.path.join(
+                self.download_dir, self.version), verbose=False)
+            self.call(["ln", "-sf", os.path.join(self.download_dir, self.version, n.split(".tar.")
+                                                 [0]), os.path.join(self.download_dir, self.version, n.split("-")[0])], shell=False, verbose=False)
+
+    def build(self, install_dir):
+        install_dir = os.path.abspath(install_dir)
+        configure_cmd = "./configure --prefix=%s -enable-checking=release --enable-languages=c,c++ --disable-multilib" % install_dir
+        make_cmd = "make -j %s" % self.threads
+        make_install_cmd = "make install -j %s" % self.threads
+        os.chdir(os.path.join(self.download_dir, self.version))
+        for c in [configure_cmd, make_cmd, make_install_cmd]:
+            self.call(c, shell=True, verbose=False)
---
+++ src/utils.py
@@ -0,0 +1,140 @@
+import os
+import sys
+import time
+import shutil
+import string
+import shutil
+import logging
+import argparse
+import subprocess
+
+from hget import hget
+
+from ._version import __version__
+
+
+class Utils(object):
+
+    cmds = []
+
+    @property
+    def loger(self):
+        return logging.getLogger()
+
+    def call(self, cmd, run=True, verbose=False, shell=True):
+        if not shell:
+            cmd_string = " ".join(cmd)
+        else:
+            cmd_string = cmd
+        self.__class__.cmds.append(cmd_string.strip())
+        if not run:
+            if verbose:
+                print(cmd_string)
+            return
+        if verbose:
+            subprocess.check_call(cmd, shell=shell, stdout=sys.stdout,
+                                  stderr=sys.stderr)
+        else:
+            with open(os.devnull, "w") as fo:
+                subprocess.check_call(cmd, shell=shell, stdout=fo, stderr=fo)
+
+    def mkdirs(self, *path):
+        for p in path:
+            if not os.path.isdir(p):
+                os.makedirs(p)
+
+    def rmpath(self, *path):
+        for p in path:
+            if os.path.isdir(p):
+                shutil.rmtree(p)
+            elif os.path.isfile(p):
+                os.remove(p)
+
+    def writecmd(self):
+        with open(os.path.join(self.workdir, "cmd.sh"), "w") as fo:
+            for cmd in self.cmds:
+                fo.write(cmd + "\n")
+
+    def uncompress(self, filename, outdir, verbose=True):
+        args = "-xf"
+        un_dir = os.path.join(
+            outdir, os.path.basename(filename).split(".tar.")[0])
+        if filename.endswith("bz2"):
+            args = "-j" + args[1:]
+        if verbose:
+            args += "v"
+        if os.path.isdir(un_dir):
+            keep = False
+            for f in os.listdir(un_dir):
+                if os.path.islink(os.path.join(un_dir, f)):
+                    os.unlink(os.path.join(un_dir, f))
+                elif os.path.isdir(os.path.join(un_dir, f)):
+                    shutil.rmtree(os.path.join(un_dir, f))
+                elif f.endswith(".bz2") or f.endswith(".gz") or f.endswith(".ht"):
+                    keep = True
+                else:
+                    os.remove(os.path.join(un_dir, f))
+            if not keep:
+                shutil.rmtree(un_dir)
+        self.call(["tar", args, filename, "-C", outdir],
+                  shell=False, verbose=verbose)
+
+
+gcc_version = [
+    'gcc-10.1.0', 'gcc-10.2.0', 'gcc-10.3.0', 'gcc-10.4.0', 'gcc-11.1.0',
+    'gcc-11.2.0', 'gcc-11.3.0', 'gcc-12.1.0', 'gcc-5.1.0', 'gcc-5.2.0',
+    'gcc-5.3.0', 'gcc-5.4.0', 'gcc-5.5.0', 'gcc-6.1.0', 'gcc-6.2.0',
+    'gcc-6.3.0', 'gcc-6.4.0', 'gcc-6.5.0', 'gcc-7.1.0', 'gcc-7.2.0',
+    'gcc-7.3.0', 'gcc-7.4.0', 'gcc-7.5.0', 'gcc-8.1.0', 'gcc-8.2.0',
+    'gcc-8.3.0', 'gcc-8.4.0', 'gcc-8.5.0', 'gcc-9.1.0', 'gcc-9.2.0',
+    'gcc-9.3.0', 'gcc-9.4.0', 'gcc-9.5.0'
+]
+
+gcc_base_url = 'https://mirrors.tuna.tsinghua.edu.cn/gnu/gcc/'
+
+
+def loger(logfile=None, level="info"):
+    logger = logging.getLogger()
+    if level.lower() == "info":
+        logger.setLevel(logging.INFO)
+        f = logging.Formatter(
+            '[%(levelname)s %(asctime)s] %(message)s')
+    elif level.lower() == "debug":
+        logger.setLevel(logging.DEBUG)
+        f = logging.Formatter(
+            '[%(levelname)s %(threadName)s %(asctime)s %(funcName)s(%(lineno)d)] %(message)s')
+    if logfile is None:
+        h = logging.StreamHandler(sys.stdout)
+    else:
+        h = logging.FileHandler(logfile, mode='w')
+    h.setFormatter(f)
+    logger.addHandler(h)
+    return logger
+
+
+desc = '''
+build and install gcc/c++/g++ version in rootless.
+
+support gcc version: 
+    ['gcc-10.1.0', 'gcc-10.2.0', 'gcc-10.3.0', 'gcc-10.4.0', 'gcc-11.1.0',
+     'gcc-11.2.0', 'gcc-11.3.0', 'gcc-12.1.0', 'gcc-5.1.0', 'gcc-5.2.0',
+     'gcc-5.3.0', 'gcc-5.4.0', 'gcc-5.5.0', 'gcc-6.1.0', 'gcc-6.2.0',
+     'gcc-6.3.0', 'gcc-6.4.0', 'gcc-6.5.0', 'gcc-7.1.0', 'gcc-7.2.0',
+     'gcc-7.3.0', 'gcc-7.4.0', 'gcc-7.5.0', 'gcc-8.1.0', 'gcc-8.2.0',
+     'gcc-8.3.0', 'gcc-8.4.0', 'gcc-8.5.0', 'gcc-9.1.0', 'gcc-9.2.0',
+     'gcc-9.3.0', 'gcc-9.4.0', 'gcc-9.5.0']
+'''
+
+
+def parseArg():
+    parser = argparse.ArgumentParser(
+        description=desc.strip())
+    parser.add_argument("-g", "--gcc", required=True,
+                        help="which gcc version to install", metavar="<str>")
+    parser.add_argument("-d", "--download-dir", required=True,
+                        help="download directory for all gcc packages", metavar="<str>")
+    parser.add_argument("-i", "--install-dir", required=True,
+                        help="install directory", metavar="<str>")
+    parser.add_argument('-v', '--version',
+                        action='version', version="v" + __version__)
+    return parser.parse_args()
